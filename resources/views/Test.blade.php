e67b4fe449b2be9cf9c6f6eaa5c24c8135dcda14
vinceExia <vincent.aubert@viacesi.fr>
2018-04-10 17:17:47 +0200

    EventController

---
+++ resources/views/Test.blade.php
@@ -0,0 +1,14 @@
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/site.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+
+</body>
+
+</html>
