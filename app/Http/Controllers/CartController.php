9d7cfe1907253cffacabd1f7c392d6b368ec880f
VuillaumeGautier <gautier.vuillaume@viacesi.fr>
2018-04-11 17:17:05 +0200

    gitingore update

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,23 +8,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Providers\Product;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+
     }
 
     public function getBoutique(){
-        $products = Product::all();
-        return view('boutique',['products' => $products]);
+
+        return view('boutique');
     }
 
-    public function add() {
-        return view('createProduct');
+    public function sortedProducts() {
+
+
+
+        $htmlAnswer = '<div class = "container">';
+
+        if($_POST["type"] =! "") {
+            $products = Product::all()->where("type","=",$_POST["type"]);
+        }
+        else{
+            $products = Product::all();
+        }
+
+        foreach ($products as $product){
+            $htmlAnswer = $htmlAnswer.'<div class="col-sm-12 col-md-6 col-lg-4">';
+        }
+
+        return $htmlAnswer;
     }
+
     public function create(Request $request){
     }
 
---
+++ resources/views/shop.blade.php
@@ -0,0 +1,23 @@
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js%22%3E"></script>
+
+</head>
+<body>
+
+<h1>Products</h1>
+
+<div>
+    Sort by <div>TYPE</div>
+</div>
+
+<div class="container">
+
+
+</div>
+
+
+</body>
+</html>
